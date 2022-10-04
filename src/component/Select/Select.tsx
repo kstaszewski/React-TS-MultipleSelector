050efe288fe9380058ecccc31481ed40a4a60899
Krzysztof <ks.staszewski@gmail.com>
2022-10-04 14:49:01 +0200

    keyboard Accesibility

--- src/component/Select/Select.tsx
+++ src/component/Select/Select.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import css from './Select.module.css';
 import { useState } from 'react';
 
@@ -26,6 +26,7 @@ type SelectProps = {
 export default function Select({ multiple, value, onChange, options }: SelectProps) {
     const [isOpen, setIsOpen] = useState(false);
     const [highlightedIndex, setHighlightedIndex] = useState(0);
+    const containerRef = useRef<HTMLDivElement>(null);
 
     const clearOptions = () => {
         multiple ? onChange([]) : onChange(undefined);
@@ -51,9 +52,42 @@ export default function Select({ multiple, value, onChange, options }: SelectPro
         if (isOpen) setHighlightedIndex(0);
     }, [isOpen]);
 
+    useEffect(() => {
+        const handler = (e: KeyboardEvent) => {
+            if (e.target != containerRef.current) return;
+            switch (e.code) {
+                case 'Enter':
+                case "Space":
+                    setIsOpen(prev => !prev);
+                    if (isOpen) selectOption(options[highlightedIndex]);
+                    break;
+                case "ArrowUp":
+                case "ArrowDown": {
+                    if (!isOpen) {
+                        setIsOpen(true);
+                        break;
+                    }
+                    const newValue = highlightedIndex + (e.code === 'ArrowDown' ? 1 : -1);
+                    if (newValue >= 0 && newValue < options.length) {
+                        setHighlightedIndex(newValue);
+                    }
+                    break;
+                }
+                case "Escape":
+                    setIsOpen(false);
+                    break;
+            }
+        };
+        containerRef.current?.addEventListener('keydown', handler);
+        return () => {
+            containerRef.current?.removeEventListener('keydown', handler);
+        };
+    }, [isOpen, highlightedIndex, options]);
+
     return (
         <>
             <div
+                ref={containerRef}
                 tabIndex={0}
                 className={css.container}
                 onBlur={() => setIsOpen(false)}
